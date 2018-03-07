1e482bfb68250774596a6bf960a068e0420b4065
5-4 <github email address>
2018-03-07 17:18:48 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code solution here
+function President(name, politicalParty, yearsInOffice, homeState) {
+	this.name = name
+	this.politicalParty = politicalParty
+	this.yearsInOffice = yearsInOffice
+	this.homeState = homeState
+
+	this.veto = () => 'NO!'
+	this.passBill = () => 'You can do that!'
+	this.doCharity = () => 'I like to help people.'
+	this.conductPressInterview = () => 'I am proud to be an American.'
+	this.sayHi = () => `Hi, my name is ${name}. I am from ${homeState}. I represent the ${politicalParty}s, and was in office ${yearsInOffice}.`
+}
